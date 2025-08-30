c4365acd7568eddb25dab16a10f4d62c50590a7c
Alexandre_Reboucas <areboucasr@icloud.com>
2025-08-30 11:42:02 -0300

    Final de node

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Node-Server
+Proposta de servidor web criado em sala de auça. Turma FS-26, Digital College Sul.
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 
-const http = require('http');
+import http from 'http';
 
-http.createServer((request, response) => {
-    response.end("Hello, World!");
-}).listen(3333, 'localhost')
-;
+const PORT = 3333;
+
+const server = http.createServer((request, response) => {
+
+    // const url = request.url;
+    // const method = request.method;
+
+    const { url, method } = request;
+
+    response.setHeader("Content-Type", "text/plain; charset=utf-8");
+
+    response.write(`
+        Requisição na url: ${url}
+        Requisição no Método: ${method}
+        `);
+});
+
+
+server.listen(PORT, 'localhost', () => {
+    console.log(`Server running at http://localhost:${PORT}.`);
+});
